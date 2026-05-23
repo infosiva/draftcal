b16c0deb26ff1af3df098fbc602c4d4eb001535a
Siva <info.siva@gmail.com>
2026-05-23 07:28:15 +0100

    feat: add BackToTop button with themed accent color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from '../../components/Footer'
 import DesignEffects from '@/components/DesignEffects'
 import AnimatedBackground from '@/components/AnimatedBackground'
 import ChatBot from '@/components/ChatBot'
+import BackToTop from '@/components/BackToTop'
 import type { BrandConfig } from '@/components/SharedNavbar'
 import CookieConsent from "../../components/CookieConsent"
 import StickyFooterCTA from "../../components/StickyFooterCTA"
@@ -120,6 +121,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <main className="flex-1 pt-16">{children}</main>
         <Footer siteName={siteConfig.name} />
         <ChatBot />
+        <BackToTop accentColor="#22c55e" />
         <CookieConsent />
         <StickyFooterCTA />
         {/* eslint-disable-next-line @next/next/no-before-interactive-script-outside-document */}
